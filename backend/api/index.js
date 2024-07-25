4d8012f5d997651c4c0976aafbeea0b331dbd853
iumahboobi <iumahboobi@gmail.com>
2024-07-25 15:31:12 +0200

    new api folder added

---
+++ backend/api/index.js
@@ -0,0 +1,41 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const productRoutes = require('../routes/productRoutes')
+const infoRoutes = require('../routes/infoRoutes')
+const userRoutes = require('../routes/userRoutes')
+const cors = require('cors');
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+const MONGODB_URI = 'mongodb://localhost:27017'
+
+
+// Middleware
+app.use(express.json({limit:'50mb'}))
+
+app.use(cors({
+    origin: 'http://localhost:3000',
+    methods: 'GET,POST,PUT,DELETE',
+    allowedHeaders: 'Content-Type,Authorization',
+  }));
+
+// Connect to MongoDB
+mongoose.connect(`${MONGODB_URI}/DressUp`, {
+useNewUrlParser:true,
+useUnifiedTopology:true
+});
+
+const db = mongoose.connection;
+db.on('error', console.error.bind(console, 'MongoDB connection error'))
+db.once('open', async () => {
+    console.log('Connected to MongoDB successfuly 🚀');
+});
+
+// Routes
+app.use('/api', productRoutes)
+app.use('/api/infos', infoRoutes)
+app.use('/api/auth',userRoutes)
+// Start server
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT} 🌐`);
+});
