1e6fbb184e82a50db1f2fab9cf01474018c15c11
iumahboobi <iumahboobi@gmail.com>
2024-07-28 20:09:58 +0200

    build updated

--- backend/api/index.js
+++ backend/api/index.js
@@ -7,15 +7,15 @@ const cors = require('cors');
 
 const app = express();
 const PORT = process.env.PORT || 5000;
-const MONGODB_URI = 'mongodb://localhost:27017/DressUp'
-require('dotenv').config();
+const MONGODB_URI = process.env.MONGODB_URI || 'mongodb://localhost:27017/DressUp'
+
 
 
 // Middleware
 app.use(express.json({limit:'50mb'}))
 
 app.use(cors({
-    origin: 'http://localhost:3000',
+    origin: process.env.FRONTEND_URL || 'http://localhost:3000',
     methods: 'GET,POST,PUT,DELETE',
     allowedHeaders: 'Content-Type,Authorization',
   }));
